4de794aad6cf79e634f7c861165e72976da81656
Maxim Perepelitsyn <mperepelitsyn@gmail.com>
2023-12-14 12:29:38 -0800

    Refactor day 14

--- src/bin/14.rs
+++ src/bin/14.rs
@@ -1,64 +1,52 @@
 use std::fs::read_to_string;
 
 fn part1(input: &str) -> u64 {
-    let grid: Vec<_> = input.lines().map(|line| line.bytes().collect::<Vec<_>>()).collect();
-    let grid = tilt(grid);
-    // for row in &grid {
-    //     println!("{}", String::from_utf8(row.clone()).unwrap());
-    // }
-    count_mass(&grid)
+    let grid = parse_input(input);
+    count_mass(&tilt(grid))
 }
 
 fn part2(input: &str) -> u64 {
-    let mut grid: Vec<_> = input.lines().map(|line| line.bytes().collect::<Vec<_>>()).collect();
-    let mut map = std::collections::HashMap::new();
-    let mut cache = std::collections::HashMap::new();
-    map.insert(grid.clone(), 0);
-    let mut offset = 0;
-    let mut start = 0;
-    let mut len = 0;
+    let mut grid = parse_input(input);
+    let mut seen_grids = std::collections::HashMap::new();
+    let mut seen_cycles = std::collections::HashMap::new();
+    seen_grids.insert(grid.clone(), 0);
 
-    for i in 1..200 {
-        let t= std::time::Instant::now();
+    let (mut start, mut offset, mut len) = (0, 0, 0);
+    for i in 1.. {
         grid = cycle(grid);
-        println!("elapsed: {:?}", t.elapsed());
+        seen_cycles.insert(i, grid.clone());
 
-        cache.insert(i, grid.clone());
-
-        if map.contains_key(&grid) {
-            if offset == 0 { offset = i; }
-            if start == 0 { start = map[&grid] }
-            else if start == map[&grid] {
-                println!("found cycle at {i}");
+        if let Some(&prev_i) = seen_grids.get(&grid) {
+            if start == 0 {
+                start = i;
+            }
+            if offset == 0 {
+                offset = seen_grids[&grid]
+            } else if offset == prev_i {
                 len = i - offset;
                 break;
             }
-
-            println!("already seen {i} -> {}", map[&grid]);
         } else {
-            map.insert(grid.clone(), i);
+            seen_grids.insert(grid.clone(), i);
         }
-
-        // for row in &grid {
-        //     println!("{}", String::from_utf8(row.clone()).unwrap());
-        // }
-        // println!("\n+++++++++++++++++++++++++++++++++++++++++++++++++\n");
     }
 
-    println!("off={offset} start={start} len={len}");
-    let grid = cache[&(((1_000_000_000 - offset) % len) as usize + start as usize)].clone();
-    count_mass(&grid)
+    let grid = &seen_cycles[&((1_000_000_000 - start) % len + offset)];
+    count_mass(grid)
 }
 
-fn cycle(grid: Vec<Vec<u8>>) -> Vec<Vec<u8>> {
-    let grid = tilt(grid);
-    let grid = rotate(grid);
-    let grid = tilt(grid);
-    let grid = rotate(grid);
-    let grid = tilt(grid);
-    let grid = rotate(grid);
-    let grid = tilt(grid);
-    let grid = rotate(grid);
+fn parse_input(input: &str) -> Vec<Vec<u8>> {
+    input
+        .lines()
+        .map(|line| line.bytes().collect::<Vec<_>>())
+        .collect()
+}
+
+fn cycle(mut grid: Vec<Vec<u8>>) -> Vec<Vec<u8>> {
+    for _ in 0..4 {
+        grid = tilt(grid);
+        grid = rotate(grid);
+    }
     grid
 }
 
@@ -79,22 +67,24 @@ fn transpose(grid: Vec<Vec<u8>>) -> Vec<Vec<u8>> {
     }
     ret
 }
+
 fn count_mass(grid: &Vec<Vec<u8>>) -> u64 {
-    grid.iter().enumerate().map(|(i, row)| {
-        row.iter().filter(|&&b| b == b'O').count() * (grid.len() - i)
-    }).sum::<usize>() as _
+    grid.iter()
+        .enumerate()
+        .map(|(i, row)| row.iter().filter(|&&b| b == b'O').count() * (grid.len() - i))
+        .sum::<usize>() as _
 }
 
 fn tilt(mut grid: Vec<Vec<u8>>) -> Vec<Vec<u8>> {
-    let mut h = vec![0; grid[0].len()];
+    let mut height = vec![0; grid[0].len()];
     for i in 0..grid.len() {
         for j in 0..grid[0].len() {
             if grid[i][j] == b'O' {
                 grid[i][j] = b'.';
-                grid[h[j]][j] = b'O';
-                h[j] += 1;
+                grid[height[j]][j] = b'O';
+                height[j] += 1;
             } else if grid[i][j] == b'#' {
-                h[j] = i + 1;
+                height[j] = i + 1;
             }
         }
     }
